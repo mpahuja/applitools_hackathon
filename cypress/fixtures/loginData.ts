8c67fd2d332d8f7972706f20347005ff5bb4dffb
Manoj Pahuja <mpahuja@lyft.com>
2019-11-06 16:04:14 -0800

    Add intial setup and v1 Tests

---
+++ cypress/fixtures/loginData.ts
@@ -0,0 +1,17 @@
+export const fieldsInputOutput = [
+    {
+        "erroMessage": "Both Username and Password must be present"
+    },
+    {
+        "password": "sdf",
+        "erroMessage": "Username must be present"
+    },
+    {
+        "userName": "sdf",
+        "erroMessage": "Password must be present"
+    },
+    {
+        "userName": "validUsername",
+        "password": "validPassword"
+    }
+]
